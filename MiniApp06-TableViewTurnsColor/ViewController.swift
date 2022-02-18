2e730062a2a19a085057e70e94dc76fea2e26c28
MaedaKouta <maedakoutaa@gmail.com>
2022-02-18 09:11:47 +0900

    commit

--- MiniApp06-TableViewTurnsColor/ViewController.swift
+++ MiniApp06-TableViewTurnsColor/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private var num = 0
+    private var nums: [Int] =  [0]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for _ in 0 ..< 200 {
+            num += 10
+            nums.append(num)
+        }
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 200
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var identifier = ""
+        
+        if indexPath.row % 5 == 0{
+            identifier = "cell1"
+        } else {
+            identifier = "cell2"
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier)!
+        cell.textLabel?.text = String(nums[indexPath.row])
+        return cell
+    }
 
 }
 
